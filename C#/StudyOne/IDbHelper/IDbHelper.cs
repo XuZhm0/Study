296495764a2ac71f8e102862c3d10bd9af801d23
paop <1392679941@qq.com>
2018-06-22 10:35:59 +0800

    commit to gitHub

---
+++ C#/StudyOne/IDbHelper/IDbHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IDbHelper {
+	interface IDbHelper {
+		//返回指定类型对象
+		T QueryForObject<T>(string sql);
+		//返回指定类型对象集合
+		IList<T> QueryForObjects<T>(string sql);
+
+		//返回指定类型对象字典
+		Dictionary<TKEY,TVALUE> QueryForDict<TKEY,TVALUE>(int column,string sql);
+		 
+		//返回Ilist<List<object>> 
+		IList<List<object>> Querys(string sql);
+
+
+		//执行Sql语句并返回成功还是失败
+		int Execute(string sql); 
+	}
+}
